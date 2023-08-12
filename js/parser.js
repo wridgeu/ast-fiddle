9e423c95f3c9a79c335728c4fbcf8c3b43acaeb5
Marco Beier <marco-beier@t-online.de>
2023-08-12 22:42:03 +0200

    chore: add lines 1:1 from course to follow along properly

--- js/parser.js
+++ js/parser.js
@@ -4,4 +4,6 @@ const exampleCode = "2 + (4 * 20)";
 
 const ast = parse(exampleCode);
 
-console.log(ast.program.body[0]);
+console.log(ast.program.body[0].expression.left.value); // 2
+console.log(ast.program.body[0].expression.right.left.value); // 4
+console.log(ast.program.body[0].expression.right.right.value); // 10
