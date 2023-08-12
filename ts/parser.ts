7d1eb295c11a2fc6f1cf2f839caf60649371e57a
Marco Beier <marco-beier@t-online.de>
2023-08-12 22:46:59 +0200

    fix type errors

--- ts/parser.ts
+++ ts/parser.ts
@@ -4,6 +4,17 @@ const exampleCode = "2 + (4 * 20)";
 
 const ast = parse(exampleCode);
 
-console.log(ast.program.body[0].expression.left.value); // 2
-console.log(ast.program.body[0].expression.right.left.value); // 4
-console.log(ast.program.body[0].expression.right.right.value); // 10
+const [statement] = ast.program.body;
+
+if (
+  statement.type === "ExpressionStatement" &&
+  statement.expression.type === "BinaryExpression" &&
+  statement.expression.left.type === "NumericLiteral" &&
+  statement.expression.right.type === "BinaryExpression" &&
+  statement.expression.right.left.type === "NumericLiteral" &&
+  statement.expression.right.right.type === "NumericLiteral"
+) {
+  console.log(statement.expression.left.value); // 2
+  console.log(statement.expression.right.left.value); // 4
+  console.log(statement.expression.right.right.value); // 10
+}
